1ff2f5d7111895ce96a9625c606fd95b5bb0b1bf
Aidan-B <31750217+Aidan-B@users.noreply.github.com>
2018-11-26 17:18:41 -0500

    Initial Git upload

---
+++ mspfunctions.js
@@ -0,0 +1,79 @@
+/*
+MultiWii Serial Protocol
+MSP is a protocol designed by the MultiWii community, with the idea to be light, generic, bit wire efficient, secure. The MSP data frames are structured as:
+
+$<header>,<direction>,<size>,<command>,<crc>$
+where:
+
+header: the ASCII characters $\$M$
+direction: the ASCII character $<$ if the message goes to the MultiWii board or $>$ if the message is coming from the board
+size: number of data bytes, binary. Can be zero as in the case of a data request to the board
+command: message id of MSP
+data: values to be sent. UINT16 values are LSB first
+crc: (cyclic redundancy check) checksum, XOR of $<size>,<command>$ and each data byte into a zero sum
+*/
+
+function int16ToInt8(num) {
+  var num8Bit1 = num & 0xff;
+  var num8Bit2 = ((num >> 8) & 0xff);
+  return [num8Bit1, num8Bit2];
+}
+
+module.exports = {
+
+  MSP_ATTITIDE: function() {
+    return [36, 77, 60, 0, 108, 108];
+  },
+
+  MSP_MOTOR: function() {
+   return [36, 77, 60, 0, 104, 104];
+  },
+
+  MSP_RC: function() {
+    return [36, 77, 60, 0, 105, 105];
+  },
+
+  MSP_SET_RAW_RC: function(throttle, roll, pitch, yaw, aux1, aux2, aux3, aux4) {
+    var out = [36, 77, 60, 16, 200];
+    var cs = 16 ^ 200;
+    var inputs = [roll, pitch, throttle, yaw, aux1, aux2, aux3, aux4];
+    inputs.forEach(function(data) {
+      int16ToInt8(data).forEach(function(value) {
+        cs ^= value;
+        out.push(value);
+      });
+    })
+    out.push(cs);
+
+    return out;
+  },
+
+  MSP_SET_MOTOR: function(m1, m2, m3, m4, m5, m6, m7, m8) {
+    var out = [36, 77, 60, 16, 214];
+    var cs = 16 ^ 214;
+    var inputs = [m1, m2, m3, m4, m5, m6, m7, m8];
+    inputs.forEach(function(data) {
+      int16ToInt8(data).forEach(function(value) {
+        cs ^= value;
+        out.push(value);
+      });
+    })
+    out.push(cs);
+
+    return out;
+  },
+
+
+  ReturnMSP_RC: function(data) {
+    var roll = data.readInt16LE(5);
+    var pitch = data.readInt16LE(7);
+    var yaw = data.readInt16LE(9);
+    var throttle = data.readInt16LE(11);
+    var aux1 = data.readInt16LE(13);
+    var aux2 = data.readInt16LE(15);
+    var aux3 = data.readInt16LE(17);
+    var aux4 = data.readInt16LE(19);
+    return {throttle: throttle, roll: roll, pitch: pitch, yaw: yaw, aux1: aux1, aux2: aux2, aux3: aux3, aux4: aux4}
+  }
+
+}
---
+++ server.js
@@ -0,0 +1,119 @@
+//server data (client-server)
+var http = require('http').createServer(handler)
+var io = require('socket.io')(http);
+var fs = require('fs');
+var serverPort = 8080;
+
+//serial data (server-flight controller)
+var mspFuncs = require('./mspfunctions.js');
+var serialport = require('serialport');
+var portLocation = '/dev/ttyUSB0';
+var serialPort = new serialport(portLocation, { baudRate : 115200 });
+
+var dataRate = 200;
+var motorData, attitude, rcData;
+var controller = {
+  throttle: 1000,
+  pitch: 1500,
+  roll: 1500,
+  yaw: 1500,
+  aux1: 1000,
+  aux2: 1000,
+  aux3: 1000,
+  aux4: 1000
+};
+
+console.log('Server Started');
+http.listen(serverPort);
+console.log('Listening on port: ' + serverPort);
+
+//Serve index.html
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+serialPort.on('open', function () {
+  console.log('Flight controller connected at: ' + portLocation);
+  setInterval(function() {
+
+    serialPort.write(mspFuncs.MSP_MOTOR(), function (err) {
+      //console.log(err);
+    });
+    serialPort.write(mspFuncs.MSP_ATTITIDE(), function (err) {
+      //console.log(err);
+    });
+    serialPort.write(mspFuncs.MSP_RC(), function (err) {
+      //console.log(err);
+    });
+  }, dataRate);
+
+  setInterval(function() {
+    serialPort.write(mspFuncs.MSP_SET_RAW_RC(controller.throttle, controller.roll, controller.pitch, controller.yaw, controller.aux1, controller.aux2, controller.aux3, controller.aux4))
+  }, 20);
+
+  //received data from flight controller
+  serialPort.on('data', function (data) {
+    if (data[4] == 104) { //MSP_MOTOR
+      //console.log('MSP_MOTOR');
+      motorData = {};
+      for (var count = 0; count < 8; count++) {
+        var motor = data.readInt16LE(5 + (count * 2));
+        if (motor != 0) {
+          motorData['m' + (count+1)] = motor;
+        }
+      }
+      //console.log(motorData);
+    }
+
+    if (data[4] == 108) { //MSP_ATTITIDE
+      //console.log('MSP_ATTITIDE');
+      var roll = data.readInt16LE(5) / 10.0;
+      var pitch = data.readInt16LE(7) / 10.0;
+      var yaw = data.readInt16LE(9);
+      attitude = {roll: roll, pitch: pitch, yaw: yaw};
+      //console.log(attitude);
+    }
+    if (data[4] == 200 && data[4] == data[5]) { //MSP_SET_RAW_RC
+      //console.log(JSON.stringify(data));
+    }
+    if (data[4] == 105) {//MSP_RC
+      rcData = mspFuncs.ReturnMSP_RC(data);
+      //console.log(rcData);
+    }
+  });
+
+
+
+
+});
+serialPort.on('close', function () {
+  console.log('Flight controller disconnected');
+  controller.aux4 = 2000; //activate FAILSAFE
+});
+
+io.on('connection', function (socket) {
+  console.log('Client connected');
+
+  setInterval(function() {
+    socket.emit('motorData', motorData);
+    socket.emit('attitude', attitude);
+    socket.emit('rcData', rcData);
+  }, dataRate);
+
+  socket.on('RC', function(data) {
+    controller = data;
+  })
+
+  socket.on('disconnect', function() {
+    console.log('Client disconnected');
+    controller.aux4 = 2000; //activate FAILSAFE
+  });
+});
